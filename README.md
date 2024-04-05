935d295fda31502a0b8933d5592f1107fc068b91
Vladyslav Avramenko <vladyslav.avramenko@itomy.ch>
2024-04-05 19:49:38 +0300

    RND-146 CORS, redirect to https, DNS record

--- README.md
+++ README.md
@@ -7,14 +7,15 @@ We highly recommend that in your code you pin the version to the exact version y
 using so that your infrastructure remains stable, and update versions in a
 systematic way so that they do not catch you by surprise.
 
-### example usage for website module:
+### Example usage for website module:
 ```
 module "gcp_website" {
-  source      = "dashdevs/static-website/gcp"
-  domain      = var.website_domain
-  name_prefix = var.name_prefix
+  source                = "dashdevs/static-website/gcp"
+  domain                = var.website_domain
+  name_prefix           = var.name_prefix
+  gcp_api_services_list = ["storage.googleapis.com", "compute.googleapis.com", "cloudresourcemanager.googleapis.com"]
+  domain_zone_name      = "exaple-domain-zone"
 }
-
 ```
 
 ## Requirements
@@ -35,9 +36,9 @@ module "gcp_website" {
 | <a name="input_domain_zone_name"></a> [domain\_zone\_name](#input\_domain\_zone\_name) | Domain zone name for CNAME record, if not empty then record will be created | `string` | `null` | no |
 | <a name="input_name_prefix"></a> [name\_prefix](#input\_name\_prefix) | Name prefix for the resources | `string` | n/a | yes |
 | <a name="input_domain"></a> [domain](#input\_website\_domain) | Domain name of the website | `string` | n/a | yes |
-
+| <a name="input_cors"></a> [cors_allowed_origins](#input\_website\_domain) | Used to declare domains from which the site will be accessed as a storage of static resources | `list(string)` | null | no |
 ## Outputs
 
 | Name | Description |
 |------|-------------|
-| <a name="output_lb_ip_address"></a> [lb\_ip\_address](#output\_lb\_ip\_address) | IP address of the website Load Balancer |
+| <a name="output_lb_ip_address"></a> [lb\_ip\_address](#output\_lb\_ip\_address) | IP address of the website Load Balancer to attach to a different DNS |
--- main.tf
+++ main.tf
@@ -25,20 +25,28 @@ resource "google_storage_bucket" "website" {
     main_page_suffix = "index.html"
     not_found_page   = "index.html"
   }
-  cors {
-    origin          = var.cors_allowed_origins
-    method          = local.cors_allowed_methods
-    response_header = ["*"]
-    max_age_seconds = 3600
+  dynamic "cors" {
+    for_each = local.сreate_cors_configuration ? [1] : []
+    content {
+      origin          = var.cors_allowed_origins
+      method          = local.cors_allowed_methods
+      response_header = ["*"]
+      max_age_seconds = 3600
+    }
   }
 
   depends_on = [local.gcp_api_services_lists]
 }
 
+resource "google_compute_global_address" "website" {
+  name = "${var.name_prefix}-lb-ip"
+}
+
 resource "google_storage_default_object_access_control" "website" {
-  bucket     = google_storage_bucket.website.name
-  role       = "OWNER"
-  entity     = "domain-${var.domain}"
+  bucket = google_storage_bucket.website.name
+  role   = "OWNER"
+  entity = "allUsers"
+
   depends_on = [local.gcp_api_services_lists]
 }
 
@@ -47,7 +55,8 @@ resource "google_compute_backend_bucket" "website" {
   description = "Contains files needed by the website"
   bucket_name = google_storage_bucket.website.name
   enable_cdn  = true
-  depends_on  = [local.gcp_api_services_lists]
+
+  depends_on = [local.gcp_api_services_lists]
 }
 
 resource "google_compute_managed_ssl_certificate" "website" {
@@ -55,29 +64,48 @@ resource "google_compute_managed_ssl_certificate" "website" {
   managed {
     domains = [var.domain]
   }
+
   depends_on = [local.gcp_api_services_lists]
 }
 
 resource "google_compute_url_map" "website" {
-  name            = "${var.name_prefix}-url-map"
-  default_service = google_compute_backend_bucket.website.self_link
-  depends_on      = [local.gcp_api_services_lists]
+  name = "${var.name_prefix}-url-map"
+  default_url_redirect {
+    host_redirect          = google_compute_backend_bucket.website.self_link
+    redirect_response_code = "MOVED_PERMANENTLY_DEFAULT" // 301 redirect
+    strip_query            = false
+    https_redirect         = true
+  }
+
+  depends_on = [local.gcp_api_services_lists]
 }
 
 resource "google_compute_target_https_proxy" "website" {
   name             = "${var.name_prefix}-target-proxy"
   url_map          = google_compute_url_map.website.self_link
   ssl_certificates = [google_compute_managed_ssl_certificate.website.self_link]
-  depends_on       = [local.gcp_api_services_lists]
+
+  depends_on = [local.gcp_api_services_lists]
 }
 
-resource "google_dns_record_set" "cname" {
+resource "google_compute_global_forwarding_rule" "rule" {
+  name                  = "${var.name_prefix}-forwarding-rule"
+  load_balancing_scheme = "EXTERNAL"
+  ip_address            = google_compute_global_address.website.address
+  ip_protocol           = "TCP"
+  port_range            = "443"
+  target                = google_compute_target_https_proxy.website.self_link
+
+  depends_on = [local.gcp_api_services_lists]
+}
+
+resource "google_dns_record_set" "record" {
   count = var.domain_zone_name != null ? 1 : 0
 
   project      = var.gcp_project_id
-  name         = var.domain
+  name         = "${var.domain}."
   managed_zone = var.domain_zone_name
-  type         = "CNAME"
+  type         = "A"
   ttl          = 300
-  rrdatas      = [google_storage_bucket.website.self_link]
+  rrdatas      = [google_compute_global_address.website.address]
 }
