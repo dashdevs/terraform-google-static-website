6c96b5299472daa41af7fbe558e1843992f20633
Vladyslav Avramenko <vladyslav.avramenko@itomy.ch>
2024-04-12 15:09:47 +0300

    RND-146 dependend services

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-# output "website_ip_address" {
-#   value = google_compute_global_address.website.address
-# }
-
-# output "test1" {
-#   value = local.gcp_existing_api_storage
-# }
-
-# output "test2" {
-#   value = local.gcp_existing_api_compute
-# }
-
-output "gcp_dependend_api_services" {
-  value = local.gcp_dependend_api_services
+output "website_ip_address" {
+  value = google_compute_global_address.website.address
 }
